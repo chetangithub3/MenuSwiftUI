4b72da9001402257645aefed8a53b9ee37366ff1
Chetan Dhowlaghar <chetan.getsmail@gmail.com>
2022-11-18 09:00:33 -0600

    Initial Commit

---
+++ MenuSwiftUI/MenuSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MenuSwiftUIApp.swift
+//  MenuSwiftUI
+//
+//  Created by Chetan Dhowlaghar on 11/18/22.
+//
+
+import SwiftUI
+
+@main
+struct MenuSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
